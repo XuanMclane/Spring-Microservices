c454e7a795d748ce377d6d6ecb02ca9ad2f0f259
XuanMclane <xuanmclane@gmail.com>
2022-02-14 13:47:24 -0500

    build docker image and docker-compose

--- currency-exchange-service/src/main/java/com/minxuan/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/minxuan/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.minxuan.microservices.currencyexchangeservice;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import java.util.Optional;
 
 @RestController
 public class CurrencyExchangeController {
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     private final Environment environment;
 
     private final CurrencyExchangeRepository repository;
@@ -26,6 +30,7 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ResponseEntity retrieveExchangeValue(
             @PathVariable String from, @PathVariable String to) {
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
         Optional<CurrencyExchange> currencyExchange = repository.findByFromAndTo(from, to);
         if (!currencyExchange.isPresent()) {
             return ResponseEntity.notFound().build();
